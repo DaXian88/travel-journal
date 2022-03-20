44c60f9988b993ec2d4c66fe46feb13228ee6f27
Khalid Safy <khalid_safy88@hotmail.com>
2022-03-20 23:57:27 +0000

    Added real image of the Great Wall and linked to unsplash

--- src/assets/data.js
+++ src/assets/data.js
@@ -8,7 +8,8 @@ const data = [
     endDate: "24 Jan, 2021",
     description:
       "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-    imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
+    imageUrl:
+      "https://images.unsplash.com/photo-1608037521277-154cd1b89191?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
   },
   {
     title: "Mount Fuji",
