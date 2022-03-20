5c41069a164c447a6a5579315fd900b757e7cb6e
Khalid Safy <khalid_safy88@hotmail.com>
2022-03-20 17:58:39 +0000

    Further styling on the TravelCard component

--- src/components/TravelCard.js
+++ src/components/TravelCard.js
@@ -9,12 +9,12 @@ function TravelCard(props) {
     <div className="travel-card-container">
       <img className="location-image" src={imageUrl} alt="location" />
       <div className="journey-details">
-        <h4>{location}</h4>
+        <h4 className="location">{location}</h4>
         <h2>{title}</h2>
         <h5>
           {startDate} - {endDate}
         </h5>
-        <p>{description}</p>
+        <p className="journey-description">{description}</p>
       </div>
     </div>
   );
