7a03eff6bd951a0072d7eed8bfa624697e10bb49
Khalid Safy <khalid_safy88@hotmail.com>
2022-03-20 16:52:37 +0000

    Completed travel-card component with styling

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <TravelCard title={data.title} />
+      <TravelCard data={data} />
     </div>
   );
 }
--- src/components/TravelCard.js
+++ src/components/TravelCard.js
@@ -1,11 +1,21 @@
 import React from "react";
+import "../styles/TravelCard.css";
 
 function TravelCard(props) {
-  const { title } = props;
+  const { title, startDate, endDate, location, imageUrl, description } =
+    props.data;
 
   return (
     <div className="travel-card-container">
-      <h2>{title}</h2>
+      <img className="location-image" src={imageUrl} alt="location" />
+      <div className="journey-details">
+        <h4>{location}</h4>
+        <h2>{title}</h2>
+        <h5>
+          {startDate} - {endDate}
+        </h5>
+        <p>{description}</p>
+      </div>
     </div>
   );
 }
